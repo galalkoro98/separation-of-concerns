3b08bbf3a7f83e3de1325ad883a48d0625c5b320
galalkoro98 <galal.koro@gmail.com>
2022-11-30 23:28:13 +0100

    exercise done

--- 6-refactoring/exercises/prompt-alert-confirm/cap-swap/src/handler.js
+++ 6-refactoring/exercises/prompt-alert-confirm/cap-swap/src/handler.js
@@ -1,3 +1,14 @@
-import { capSwap } from './utils.js';
+import { capSwap } from './util.js';
 
-export const capSwapHandler = () => {};
+export const capSwapHandler = () => {
+  // read & process user input
+  let userInput = null;
+  while (userInput === null) {
+    userInput = prompt('enter a string to cap-swap');
+  }
+
+  // communicate result to user
+  //   alert(capSwapped);
+  const swapHandler = capSwap(userInput);
+  alert(swapHandler);
+};
