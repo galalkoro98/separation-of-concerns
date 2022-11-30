b6bf44a16d2b7bb2daf7cd4039349a1915b56aae
galalkoro98 <galal.koro@gmail.com>
2022-11-30 23:26:30 +0100

    exercise done

--- 6-refactoring/exercises/dom-and-events/flip-book/src/util.js
+++ 6-refactoring/exercises/dom-and-events/flip-book/src/util.js
@@ -1,4 +1,13 @@
 /**
  *
  */
-export const reverseAndUpper = () => {};
+// export const reverseAndUpper = () => {
+//     const
+// };
+
+export const reverseAndUpper = (text) =>
+  text.split('').reverse().join('').toUpperCase();
+
+// export const reverseAndUpper = (text) => {
+//     return text.split('').reverse().join('').toUpperCase();
+//   };
